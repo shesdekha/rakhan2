894796ba54b62f2bb548926c8505cb51aa519b8e
rakhan2@myseneca <rakhan2@myseneca>
2020-01-30 17:24:12 -0500

    first file

---
+++ if.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Name: Rajib Khan
+# Class: OPS435
+# Section: A
+
+# Description: This program will output the "If condition"
+
+name = 'Rajib'
+
+age = 3000
+
+if name == 'Rajib':
+
+	print('hello, Rajib!')
+
+elif age < 12:
+
+	print('You are not Rajib')
+
+elif age > 2000:
+	
+	print('Unlike you, Rajib is not an undead, Immortal vampire.')
+
+elif age > 100:
+
+	print('You are Rajib, grannie.')
